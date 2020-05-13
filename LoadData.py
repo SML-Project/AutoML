3c6faf32c4eb3b6565518aa7f5d900c1a5056f0b
loginfield <18810186339@163.com>
2020-05-13 19:45:36 +0800

    new Models and LoadData

--- LoadData.py
+++ LoadData.py
@@ -1,29 +1,6 @@
 import torch
 import numpy as np
-from torchvision.datasets import mnist,CIFAR10
-from torchvision import transforms
-from torch.utils.data import DataLoader
-import torch.utils.data as Data
 
-'''
-a general method to load various type of data from external files
-'''
-'''
-def LoadData(N, batch_size, num_workers):
-    X = torch.unsqueeze(torch.rand(N) * 4 - 2, dim=1)
-    Y = X * (8 * X).sin() * (4 / X).sin()
-    X_train, X_valid = X.split([int(0.7*N), int(0.3*N)], dim=0)
-    Y_train, Y_valid = Y.split([int(0.7*N), int(0.3*N)], dim=0)
-    data_train = TensorDataset(X_train, Y_train)
-    data_valid = TensorDataset(X_valid, Y_valid)
-    data = {"train": data_train, "valid": data_valid}
-    for phase in ["train", "valid"]:
-        print(phase + "_data: " + str(len(data[phase])))
-    print("\n")
-    loaders = {phase: DataLoader(data[phase], batch_size=batch_size, shuffle=True,  num_workers=num_workers)
-                for phase in ["train", "valid"]}
-    return loaders
-'''
 def get_data_loader(dataset_name='MNIST',batch_size=32,train=True,transform=transforms.ToTensor()):
     dataset=None
     if dataset_name=='MNIST':
@@ -49,6 +26,27 @@ def get_data_loader(dataset_name='MNIST',batch_size=32,train=True,transform=tran
             dataset = Data.TensorDataset(data, label)
         else:
             data, label = torch.load('./cifar10_dataset/data/CIFAR5_test.pt')
+            dataset = Data.TensorDataset(data, label)
+    elif dataset_name=='Quadratic_Origin':
+        if train==True:
+            data, label = torch.load('./quadratic_dataset/origin/training.pt')
+            dataset = Data.TensorDataset(data, label)
+        else:
+            data, label = torch.load('./quadratic_dataset/origin/test.pt')
+            dataset = Data.TensorDataset(data, label)
+    elif dataset_name=='Quadratic_Uniform':
+        if train==True:
+            data, label = torch.load('./quadratic_dataset/uniform/training.pt')
+            dataset = Data.TensorDataset(data, label)
+        else:
+            data, label = torch.load('./quadratic_dataset/uniform/test.pt')
+            dataset = Data.TensorDataset(data, label)
+    elif dataset_name=='Quadratic_Gauss':
+        if train==True:
+            data, label = torch.load('./quadratic_dataset/gauss/training.pt')
+            dataset = Data.TensorDataset(data, label)
+        else:
+            data, label = torch.load('./quadratic_dataset/gauss/test.pt')
             dataset = Data.TensorDataset(data, label)
     if dataset==None:
         raise ('dataset is None')
--- Models.py
+++ Models.py
@@ -2,13 +2,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+class Qua(nn.Module):
+    def __init__(self,input_units):
+        super(Qua, self).__init__()
+        self.input_units=input_units
+        self.out=nn.Linear(self.input_units,10)
+
+    def forward(self,x):
+
+        in_size = x.size(0)  # one batch     此时的x是包含batchsize维度为4的tensor，即(batchsize，channels，x，y)，x.size(0)指batchsize的值    把batchsize的值作为网络的in_size
+        x = x.view(in_size, -1)  # flatten the tensor 相当于reshape
+        x = self.out(x)
+        # print(x.size())
+        # x:64*10
+        return x # 64*10
 
 class TwoLayerNet(nn.Module):
-    def __init__(self,hidden_units,activation='sigmoid'):
+    def __init__(self,input_units,hidden_units,activation='sigmoid'):
         super(TwoLayerNet, self).__init__()
         self.activation=activation
         self.hidden_1= hidden_units
-        self.fc_1 = nn.Linear(784,self.hidden_1 )  # （in_features, out_features）
+        self.input_units=input_units
+        self.fc_1 = nn.Linear(self.input_units,self.hidden_1 )  # （in_features, out_features）
         self.out=nn.Linear(self.hidden_1,10)
 
     def forward(self,x):
@@ -29,11 +44,12 @@ class TwoLayerNet(nn.Module):
         return x # 64*10
 
 class ThreeLayerNet(nn.Module):
-    def __init__(self,hidden_units,activation='sigmoid'):
+    def __init__(self,input_units,hidden_units,activation='sigmoid'):
         super(ThreeLayerNet, self).__init__()
+        self.input_units=input_units
         self.hidden_1= hidden_units
         self.hidden_2 = hidden_units
-        self.fc_1 = nn.Linear(784,self.hidden_1 )  # （in_features, out_features）
+        self.fc_1 = nn.Linear(input_units,self.hidden_1 )  # （in_features, out_features）
         self.fc_2 = nn.Linear(self.hidden_1 , self.hidden_2)  # （in_features, out_features)
         self.out=nn.Linear(self.hidden_2,10)
 
@@ -58,30 +74,33 @@ class ThreeLayerNet(nn.Module):
 
         return x # 64*10
 
-class ConvNet(nn.Module):
+class ConvMNISTNet(nn.Module):
     def __init__(self):
-        super(ConvNet, self).__init__()
+        super(ConvMNISTNet, self).__init__()
         # 输入1通道，输出10通道，kernel 5*5
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, 5)
-        self.conv3 = nn.Conv2d(20, 40, 3)
-        self.mp = nn.MaxPool2d(2)
+        self.convlayer=nn.Sequential(
+            nn.Conv2d(in_channels=1, out_channels=10, kernel_size=5),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(10, 20, 5),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(20, 40, 3),
+            nn.ReLU(True),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
         # fully connect
-        self.fc = nn.Linear(40, 10)#（in_features, out_features）
+        self.fclayer = nn.Linear(40, 10)#（in_features, out_features）
 
     def forward(self, x):
         # in_size = 64
         in_size = x.size(0) # one batch     此时的x是包含batchsize维度为4的tensor，即(batchsize，channels，x，y)，x.size(0)指batchsize的值    把batchsize的值作为网络的in_size
         # x: 64*1*28*28
-        x = F.relu(self.mp(self.conv1(x)))
-        # x: 64*10*12*12  feature map =[(28-4)/2]^2=12*12
-        x = F.relu(self.mp(self.conv2(x)))
-        # x: 64*20*4*4
-        x = F.relu(self.mp(self.conv3(x)))
+        x = self.convlayer(x)
         x = x.view(in_size, -1) # flatten the tensor 相当于resharp
         # print(x.size())
         # x: 64*320
-        x = self.fc(x)
+        x = self.fclayer(x)
         # x:64*10
         # print(x.size())
         return x #64*10
@@ -89,69 +108,33 @@ class ConvNet(nn.Module):
 class ConvCIFARNet(nn.Module):
     def __init__(self):
         super(ConvCIFARNet, self).__init__()
-        self.conv1 = nn.Conv2d(3, 6, 5)
-        self.pool = nn.MaxPool2d(2, 2)
-        self.conv2 = nn.Conv2d(6, 16, 3)
-        self.conv3 = nn.Conv2d(16, 32, 3)
+        self.convlayer=nn.Sequential(
+            nn.Conv2d(3, 6, 5),
+            nn.BatchNorm2d(6),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
 
-        self.fc1 = nn.Linear(128, 32,bias=True)
-        self.out = nn.Linear(32, 10,bias=True)
+            nn.Conv2d(6, 16, 3),
+            nn.BatchNorm2d(16),
+            nn.ReLU(True),
+            nn.MaxPool2d(2, 2),
+
+            nn.Conv2d(16, 32, 3),
+            nn.BatchNorm2d(32),
+            nn.ReLU(True),
+            nn.MaxPool2d(2,2)
+        )
+
+        self.fclayer=nn.Sequential(
+            nn.Linear(128, 32, bias=True),
+            nn.Linear(32, 10, bias=True)
+        )
+ 
 
     def forward(self, x):
         in_size = x.size(0)
-        x = self.pool(F.relu(self.conv1(x)))
-        x = self.pool(F.relu(self.conv2(x)))
-        x = self.pool(F.relu(self.conv3(x)))
         #print(x.size())
+        x=self.convlayer(x)
         x = x.view(in_size, -1)
-        x = F.relu(self.fc1(x))
-        x = self.out(x)
+        x=self.fclayer(x)
         return x
-
-class TwoLayerCIFARNet(nn.Module):
-    def __init__(self,hidden_units,activation='sigmoid'):
-        super(TwoLayerCIFARNet, self).__init__()
-        self.hidden_1= hidden_units
-        self.fc_1 = nn.Linear(3*32*32,self.hidden_1 )  # （in_features, out_features）
-        self.out=nn.Linear(self.hidden_1,10)
-
-    def forward(self,x):
-        in_size = x.size(0)  # one batch     此时的x是包含batchsize维度为4的tensor，即(batchsize，channels，x，y)，x.size(0)指batchsize的值    把batchsize的值作为网络的in_size
-        x = x.view(in_size, -1)  # flatten the tensor 相当于resharp
-        # print(x.size())
-        # x: 64*320
-        if self.activation=='sigmoid':
-            x = torch.sigmoid(self.fc_1(x))
-        else:
-            x=F.relu(self.fc_1(x))
-        # print(x.size())
-        # x:64*20
-        x = self.out(x)
-        # print(x.size())
-        # x:64*10
-
-        return x # 64*10
-
-class ThreeLayerCIFARNet(nn.Module):
-    def __init__(self,hidden_units,activation='sigmoid'):
-        super(ThreeLayerCIFARNet, self).__init__()
-        self.hidden_1= hidden_units
-        self.hidden_2 = hidden_units
-        self.fc_1 = nn.Linear(3*32*32,self.hidden_1 )  # （in_features, out_features）
-        self.fc_2 = nn.Linear(self.hidden_1 , self.hidden_2)  # （in_features, out_features)
-        self.out=nn.Linear(self.hidden_2,10)
-
-    def forward(self,x):
-        in_size = x.size(0)  # one batch     此时的x是包含batchsize维度为4的tensor，即(batchsize，channels，x，y)，x.size(0)指batchsize的值    把batchsize的值作为网络的in_size
-        x = x.view(in_size, -1)  # flatten the tensor 相当于resharp
-        # print(x.size())
-        # x: 64*320
-        x = torch.sigmoid(self.fc_1(x))
-        # print(x.size())
-        # x:64*20
-        x = torch.sigmoid(self.fc_2(x))
-        x = self.out(x)
-        # print(x.size())
-        # x:64*10
-
-        return x # 64*10
